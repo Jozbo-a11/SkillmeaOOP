a5af5460014eccb403c0ac9d48ead619d8bec082
Jozbo-a11 <jozef.boko@gmail.com>
2025-06-16 18:03:42 +0200

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Random nahCislo = new Random();
+
+        RodinnyDom domNovakov = new RodinnyDom();
+        domNovakov.pocetPoschodi = 4;
+        domNovakov.rozloha = 457f;
+        domNovakov.material = "mramor";
+        domNovakov.vypocetCenyDomu(300, "Novakovi");
+
+
+        RodinnyDom domZajacov = new RodinnyDom();
+        domZajacov.rozloha = 646f;
+        domZajacov.pocetPoschodi = 3;
+        domZajacov.vypocetCenyDomu(250, "Zajacovi");
+
+
+
     }
 }
---
+++ src/RodinnyDom.java
@@ -0,0 +1,13 @@
+public class RodinnyDom {
+    int pocetPoschodi;
+    String material;
+    float rozloha;
+    int pocetOkien;
+
+    public void vypocetCenyDomu(int cenaZaMeter, String menoRodiny){
+        float cena = rozloha * cenaZaMeter * pocetPoschodi;
+        System.out.println(menoRodiny+ " " + cena);
+
+    }
+
+}
